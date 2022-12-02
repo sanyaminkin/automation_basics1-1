518defcd4499b9c50d67218aa769697ec40ef1bf
sanyaminkin <sanyaminkin@gmail.com>
2022-12-02 21:35:15 +0300

    CashbackHackServiceTest modified

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -38,7 +38,7 @@ public class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int amount = 1001;
         int actual = service.remain(amount);
-        int expected = 0;
+        int expected = 999;
         assertEquals(expected, actual);
     }
 
